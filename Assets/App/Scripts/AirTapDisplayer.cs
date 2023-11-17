93274befe1d288a35f550ea6df10c2756362e832
Joost van Schaik <joostvanschaik@outlook.com>
2023-11-17 14:07:08 +0100

    Get the end of the hand ray at an air tap

--- Assets/App/Scripts/AirTapDisplayer.cs
+++ Assets/App/Scripts/AirTapDisplayer.cs
@@ -1,3 +1,4 @@
+using System;
 using MixedReality.Toolkit;
 using MixedReality.Toolkit.Input;
 using MixedReality.Toolkit.Subsystems;
@@ -25,15 +26,22 @@ public class AirTapDisplayer : MonoBehaviour
         handsAggregatorSubsystem = XRSubsystemHelpers.GetFirstRunningSubsystem<IHandsAggregatorSubsystem>();
         leftHand.SetActive(false);
         rightHand.SetActive(false);
+
         
         findingService = ServiceManager.Instance.GetService<IMRTK3ConfigurationFindingService>();
         
+        var rightLineRenderer = findingService.RightHand.gameObject.GetComponentInChildren<LineRenderer>(true);
+        var leftLineRenderer = findingService.LeftHand.gameObject.GetComponentInChildren<LineRenderer>(true);
         findingService.LeftHandStatusTriggered.AddListener(t=>
         {
             leftHand.SetActive(t);
             if (t)
             {
+                var rayPos = leftLineRenderer.GetPosition(leftLineRenderer.positionCount -1);
                 textMesh.text = $"Left hand position: {GetPinchPosition(findingService.LeftHand)}";
+                textMesh.text +=
+                    $"{Environment.NewLine} Left hand ray position: {rayPos}";
+                leftHand.transform.position = rayPos;
             }
         });
         
@@ -42,7 +50,11 @@ public class AirTapDisplayer : MonoBehaviour
             rightHand.SetActive(t);
             if (t)
             {
+                var rayPos = rightLineRenderer.GetPosition(rightLineRenderer.positionCount -1);
                 textMesh.text = $"Right hand position: {GetPinchPosition(findingService.RightHand)}";
+                textMesh.text +=
+                    $"{Environment.NewLine} Right hand ray position: {rayPos}";
+                rightHand.transform.position = rayPos;
             }
         });
     }
